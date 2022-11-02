4d605551c8734c49fd1520de20e1ec8c78d712e1
Bruno Silva <brunosilva@viggosistemas.com.br>
2022-11-02 17:54:13 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="w-2/5 bg-green-500 absolute left-0 top-0 h-full p-20">
+    <h2 class="text-white text-4xl text-left font-bold mt-20">
+      Entre agora para começar a curtir as suas músicas preferidas.
+    </h2>
+  </section>
+  <section class="w-3/5 bg-white absolute right-0 top-0 h-full">
+    <div>
+      <img
+        class="m-auto scale-[0.5]"
+        src="./assets/spotify-logo.svg"
+        alt="Logo Spotify"
+      />
+    </div>
+    <div class="text-center absolute w-full bottom-16">
+      <button
+        class="bg-green-500 text-white text-bold text-lg px-8 py-4 rounded-full"
+      >
+        Entrar com Spotify
+      </button>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style></style>
