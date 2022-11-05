4de4cec0e01ec77fc494f75d81c63282222c2140
Bruno Silva <brunosilva@viggosistemas.com.br>
2022-11-04 23:50:58 -0300

    feat: Criada lógica para recuperar token de acesso de usuário.

--- src/pages/Playlists.vue
+++ src/pages/Playlists.vue
@@ -3,7 +3,40 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      url: import.meta.env.VITE_URL_TOKEN,
+    };
+  },
+  mounted() {
+    const clientID = import.meta.env.VITE_CLIENT_ID;
+    const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+    let params = new URLSearchParams(document.location.search);
+    let code = params.get("code");
+
+    let body = {
+      grant_type: "authorization_code",
+      code /* como o atributo tem mesmo nome, a cosinhsa agem como se nada tivesse acontedifo */,
+      redirect_uri: import.meta.env.VITE_REDIRECT_URI,
+    };
+
+    console.table(body);
+
+    // request post
+    axios({
+      method: "POST",
+      url: import.meta.env.VITE_URL_TOKEN,
+      data: new URLSearchParams(Object.entries(body)).toString(),
+      headers: {
+        Authorization: `Basic ${btoa(clientID + ":" + clientSecret)}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+  },
+};
 </script>
 
 <style></style>
